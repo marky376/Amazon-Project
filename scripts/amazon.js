f878545f6122d9ae3be70ecb6aa712f3b677f0bc
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-30 00:27:19 +0300

    Event Listener

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,7 @@ products.forEach((product) => {
                 Added
                 </div>
 
-                <button class="add-to-cart-button button-primary">
+                <button class="add-to-cart-button button-primary js-add-to-cart">
                 Add to Cart
                 </button>
                 </div>
@@ -56,3 +56,9 @@ products.forEach((product) => {
 console.log(productsHTML);
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+        button.addEventListener('click', () => {
+
+        });
+});
