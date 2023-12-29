9251f25f87856c6ac39341f3266d50d649113e8d
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-30 01:59:44 +0300

    Errors

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,7 +76,13 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
                                 productId: productId,
                                 quantity: 1,
                         });
-                        console.log(cart);
                 }
+                let cartQuantity = 0;
+
+                cart.forEach((item) => {
+                        cartQuantity += item.quantity;
+                });
+                
+                document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
         });
 });
