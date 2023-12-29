3db114870d6d8ef406df4b16da9827d4caa611ce
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-30 01:40:57 +0300

    Unique Names with Product ID

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,7 @@ products.forEach((product) => {
                 Added
                 </div>
 
-                <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
+                <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
                 Add to Cart
                 </button>
                 </div>
@@ -57,12 +57,12 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         button.addEventListener('click', () => {
-                const productName = button.dataset.productName;
+                const productId = button.dataset.productId;
 
                 let matchingItem;
 
                 cart.forEach((item) => {
-                        if (productName === item.productName) {
+                        if (productId === item.productId) {
                                 matchingItem = item;
                         }
                 });
@@ -73,7 +73,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
                         return;
                 } else {
                         cart.push({
-                                productName: productName,
+                                productId: productId,
                                 quantity: 1,
                         });
                         console.log(cart);
