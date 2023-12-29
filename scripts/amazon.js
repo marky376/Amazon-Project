1ee7726054cfde9f3fa9a96e1ab0d084eafb3b62
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-29 23:27:08 +0300

    Add Items in an array

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,1 +1,20 @@
-console.log('Hello');
+const products = [
+        {
+                image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+                name: 'Athletic Cotton Socks 6 Pairs',
+                rating: {
+                        stars: 4.5,
+                        count: 87
+                },
+                price: 1090
+        },
+        {
+                image: 'images/products/intermediate-composite-basketball.jpg',
+                name: 'Intermediate Composite Basketball',
+                rating: {
+                        stars: 4,
+                        count: 127
+                },
+                price: 2095
+        }
+];
