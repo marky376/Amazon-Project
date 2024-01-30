27d80fca99fb90061b636cc23bb4c42f1ec5dd01
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-01-31 00:01:28 +0300

    Fix product price display

--- scripts/index.js
+++ scripts/index.js
@@ -49,7 +49,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-price">
-                $${product.priceCents / 100}
+                $${(product.priceCents / 100).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
