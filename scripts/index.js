3fcd9ca9893d5e3de705cf9e40fd3129fd437e55
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-01-31 00:34:22 +0300

    Add event listener to "Add to Cart" button

--- scripts/index.js
+++ scripts/index.js
@@ -43,10 +43,16 @@ products.forEach((product) => {
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart">
                 Add to Cart
             </button>
         </div>
     `;
 });
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log('Added to Cart')
+    });
+});
