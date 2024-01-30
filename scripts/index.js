2656522b6504710a2fc9bd79d772a1902f736c7a
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-01-30 22:59:32 +0300

    Add new products to index.js

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+const products = [
+        {
+                image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+                name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+                rating: {
+                        stars: 4.5,
+                        count: 87
+                },
+                priceCents: 1090
+        },
+        {
+                image: 'images/products/intermediate-composite-basketball.jpg',
+                name: 'Intermediate Size Basketball',
+                rating: {
+                        stars: 4.0,
+                        count: 127
+                },
+                priceCents: 2095
+        },
+        {
+                image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+                name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+                rating: {
+                        stars: 4.5,
+                        count: 56
+                },
+                priceCents: 799
+        }
+]
